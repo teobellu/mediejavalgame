8fa96814d46fe3d7ac461c3caf9cc282a8e97b51
Jacopo Bartolini <jacopo.bartolini@mail.polimi.it>
2017-06-05 22:44:45 +0200

    wip gui

--- src/main/java/client/gui/CustomConfigController.java
+++ src/main/java/client/gui/CustomConfigController.java
@@ -4,9 +4,8 @@ import java.io.File;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -21,7 +20,7 @@ public class CustomConfigController {
 	private Button _cancelButton;
 	
 	@FXML
-	private TextField _textField;
+	private Button _fileButton;;
 	
 	private Stage _dialogStage;
 	
@@ -34,13 +33,14 @@ public class CustomConfigController {
 	}
 	
 	@FXML
-	private void handleTextFieldClicked(){
+	private void handleFileButtonClicked(){
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
 		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("XML Files", "*.xml"));
 		File selectedFile = fileChooser.showOpenDialog(_dialogStage);
-		if(selectedFile.exists()){
-			_textField.setText(selectedFile.getAbsolutePath());
+		if(selectedFile!=null && selectedFile.exists()){
+			_fileButton.setText(selectedFile.getAbsolutePath());
+			//TODO passare il file, o il suo path quanto meno, a chi deve gestirlo
 		} else {
 			Alert alert = new Alert(AlertType.ERROR);
 			alert.initOwner(_dialogStage);
