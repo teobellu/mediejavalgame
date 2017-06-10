f85337081aee390c0640fba75af3d9d4b7ad87ae
teobellu <matteo.bellusci@mail.polimi.it>
2017-06-10 12:57:26 +0200

    prova per conflitti

--- src/main/java/game/LeaderCard.java
+++ src/main/java/game/LeaderCard.java
@@ -8,10 +8,8 @@ public class LeaderCard implements ICard{
 	private String name;
 	private Effect effect;
 	private Function<Player, Boolean> requirement;
-	private boolean activable;
 	
 	public LeaderCard(String name, Effect effect, Function<Player, Boolean> requirement) {
-		setActivable(true);
 		this.name = name;
 		this.effect = effect;
 		if (effect != null)
@@ -33,14 +31,6 @@ public class LeaderCard implements ICard{
 
 	public Effect getEffect() {
 		return effect;
-	}
-
-	public boolean isActivable() {
-		return activable;
-	}
-
-	public void setActivable(boolean activable) {
-		this.activable = activable;
 	}
 	
 }
--- src/main/java/misc/ServerRemote.java
+++ src/main/java/misc/ServerRemote.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 
 import server.ConnectionHandler;
 
+@FunctionalInterface
 public interface ServerRemote extends Remote {
 
 	public ConnectionHandler onConnect() throws RemoteException;
